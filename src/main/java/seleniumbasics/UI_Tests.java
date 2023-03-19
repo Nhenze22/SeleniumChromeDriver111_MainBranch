6120f1c000b102df75fbe1fd889a9f972a497439
Nadia <nhenze22@gmail.com>
2023-03-19 18:25:17 -0400

    Chrome V111 Fixed

--- src/main/java/seleniumbasics/UI_Tests.java
+++ src/main/java/seleniumbasics/UI_Tests.java
@@ -1,11 +1,17 @@
 package seleniumbasics;
 
+import org.jetbrains.annotations.TestOnly;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class TC01_LaunchChromeBrowser {
-    public static void main(String [] args){
+public class UI_Tests {
+
+    private WebDriver driver;
+
+    @TestOnly
+
+    void openGoogle(){
         //Browser driver exe file to open browser Chrome Driver
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\gigac\\Downloads\\chromedriver.exe");
         ChromeOptions co = new ChromeOptions();
@@ -14,7 +20,9 @@ public class TC01_LaunchChromeBrowser {
         WebDriver driver=new ChromeDriver(co);
         //Open Chrome Browser
         driver.get("https://www.google.com");
-        System.out.println(driver.getTitle());
+
+
+        //System.out.println(driver.getTitle());
     }
 
 }
